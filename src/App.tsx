282a7907140d78fece50c651f88752e2c1a8d263
little_ice <2902978956@qq.com>
2020-07-22 20:38:32 +0800

    初步完成导航栏布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,89 @@
 import React from 'react';
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect
+} from 'react-router-dom';
+import styled from 'styled-components';
 
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+`;
+const Main = styled.div`
+    flex-grow: 1;
+    overflow: auto;
+`;
+const Nav = styled.nav`
+    border: solid 1px green;
+    > ul{
+      display: flex;
+      >li{
+        width: 33.33333%;
+        text-align: center;
+        padding: 16px;
+      }
+    }
+`;
 
 function App() {
-  return (
-    <div>hello</div>
-  );
+    return (
+        <Router>
+            <Wrapper>
+                <Main>
+                    <Switch>
+                        <Route path="/tags">
+                            <Tags/>
+                        </Route>
+                        <Route path="/money">
+                            <Money/>
+                        </Route>
+                        <Route path="/statistics">
+                            <Statistics/>
+                        </Route>
+                        <Redirect exact from="/" to="money"/>
+                        <Route path="*">
+                            <NoMatch/>
+                        </Route>
+                    </Switch>
+                </Main>
+                <Nav>
+                    <ul>
+                        <li>
+                            <Link to="/tags">标签页</Link>
+                        </li>
+                        <li>
+                            <Link to="/money">记账页</Link>
+                        </li>
+                        <li>
+                            <Link to="/statistics">统计页</Link>
+                        </li>
+                    </ul>
+                </Nav>
+
+
+            </Wrapper>
+        </Router>
+    );
+}
+
+function NoMatch() {
+    return <h2>地址输入错误，页面不存在！</h2>;
+}
+
+function Statistics() {
+    return <h2>统计页面</h2>;
+}
+
+function Tags() {
+    return <h2>标签页面</h2>;
+}
+
+function Money() {
+    return <h2>记账页面</h2>;
 }
 
 export default App;
