1da7cd016ffde58913cc182ee0c47cb074ad232d
little_ice <2902978956@qq.com>
2020-07-23 09:00:18 +0800

    添加 Icon 组件

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,7 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
-require('icons/tag.svg')
-require('icons/chart.svg')
-require('icons/money.svg')
+import Icon from './Icon';
 
 const NavWrapper = styled.nav`
     line-height: 24px;
@@ -26,30 +24,24 @@ const NavWrapper = styled.nav`
     }
 `;
 
-const Nav = ()=>{
-    return(
+const Nav = () => {
+    return (
         <NavWrapper>
             <ul>
                 <li>
-                    <svg className="icon">
-                        <use xlinkHref="#tag"/>
-                    </svg>
+                    <Icon name='tag'/>
                     <Link to="/tags">标签页</Link>
                 </li>
                 <li>
-                    <svg className="icon">
-                        <use xlinkHref="#money"/>
-                    </svg>
+                    <Icon name='money'/>
                     <Link to="/money">记账页</Link>
                 </li>
                 <li>
-                    <svg className="icon">
-                        <use xlinkHref="#chart"/>
-                    </svg>
+                    <Icon name='chart'/>
                     <Link to="/statistics">统计页</Link>
                 </li>
             </ul>
         </NavWrapper>
-    )
-}
-export default Nav
+    );
+};
+export default Nav;
