440c0b63e9a933e1f7248a11cd6a3c0d3b1cb2fe
little_ice <2902978956@qq.com>
2020-07-23 09:23:21 +0800

    添加 Layout 组件 ，创建 views 目录

--- src/App.tsx
+++ src/App.tsx
@@ -3,63 +3,35 @@ import {
     HashRouter as Router,
     Switch,
     Route,
-    Link,
     Redirect
 } from 'react-router-dom';
-import styled from 'styled-components';
-import Nav from './components/Nav';
 
-const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-`;
-const Main = styled.div`
-    flex-grow: 1;
-    overflow: auto;
-`;
+import Money from '../views/Money';
+import Statistics from '../views/Statistics';
+import Tags from '../views/Tags';
+import NoMatch from '../views/NoMatch';
+
 
 function App() {
     return (
         <Router>
-            <Wrapper>
-                <Main>
-                    <Switch>
-                        <Route path="/tags">
-                            <Tags/>
-                        </Route>
-                        <Route path="/money">
-                            <Money/>
-                        </Route>
-                        <Route path="/statistics">
-                            <Statistics/>
-                        </Route>
-                        <Redirect exact from="/" to="money"/>
-                        <Route path="*">
-                            <NoMatch/>
-                        </Route>
-                    </Switch>
-                </Main>
-                <Nav />
-            </Wrapper>
+            <Switch>
+                <Route path="/tags">
+                    <Tags/>
+                </Route>
+                <Route path="/money">
+                    <Money/>
+                </Route>
+                <Route path="/statistics">
+                    <Statistics/>
+                </Route>
+                <Redirect exact from="/" to="money"/>
+                <Route path="*">
+                    <NoMatch/>
+                </Route>
+            </Switch>
         </Router>
     );
-}
-
-function NoMatch() {
-    return <h2>地址输入错误，页面不存在！</h2>;
-}
-
-function Statistics() {
-    return <h2>统计页面</h2>;
-}
-
-function Tags() {
-    return <h2>标签页面</h2>;
-}
-
-function Money() {
-    return <h2>记账页面</h2>;
 }
 
 export default App;
