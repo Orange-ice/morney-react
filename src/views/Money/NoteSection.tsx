39b8b4b04c0abb51ea931217b6709c2d650bd214
little_ice <2902978956@qq.com>
2020-07-24 22:22:18 +0800

    Tag.tsx 的样式完成

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,23 +1,11 @@
 import styled from 'styled-components';
-import React, {ChangeEventHandler, useRef} from 'react';
+import React, {ChangeEventHandler} from 'react';
 import {Input} from 'components/Input';
 
 const Wrapper = styled.section`
   background: #f5f5f5;
-  padding: 0 16px;
+  padding: 14px 16px;
   font-size: 14px;
-  >label{
-    display: flex;
-    align-items: center;
-    > span{margin-right: 16px;white-space: nowrap;}
-    >input{
-    display: block;
-    width: 100%;
-    height: 72px;
-    background:none;
-    border: none;
-    }
-  }
 `;
 type Props = {
     value:string,
@@ -30,7 +18,7 @@ const NoteSection:React.FC<Props> = (props)=>{
     }
     return(
         <Wrapper>
-            <Input label="备注" type="text" value={note} onChange={onChange} />
+            <Input label="备注" type="text" value={note} onChange={onChange} placeholder="点我输入备注噢" />
         </Wrapper>
     )
 }
