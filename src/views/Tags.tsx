65c6c6901afa5e7209a04f9e7bd47e984393dcaa
little_ice <2902978956@qq.com>
2020-07-24 21:50:59 +0800

    创建 单独 的 Button 组件

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,9 +1,10 @@
-import Layout from '../components/Layout';
+import Layout from 'components/Layout';
 import React from 'react';
 import {useTags} from 'useTags';
 import styled from 'styled-components';
-import Icon from '../components/Icon';
+import Icon from 'components/Icon';
 import {Link} from 'react-router-dom';
+import {Button} from 'components/Button';
 
 
 const TagList = styled.ol`
@@ -23,14 +24,7 @@ const TagList = styled.ol`
   }
 `;
 
-const Button = styled.button`
-  border: none;
-  font-size: 18px;
-  padding: 8px 12px;
-  background: #ff6600;
-  border-radius: 4px;
-  color: white;
-`
+
 const Center = styled.div`
   display: flex;
   justify-content: center;
