82e5fdaf06b9ae397b175bc7b7b9b826ba066561
little_ice <2902978956@qq.com>
2020-07-25 17:56:09 +0800

    Tag.tsx 里 编辑 tag 名的实现

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -2,15 +2,33 @@ import {useState} from 'react';
 import {createId} from './lib/createId';
 
 const defaultTags = [
-    {id:createId(),name:'服饰'},
-    {id:createId(),name:'交通'},
-    {id:createId(),name:'通讯'},
-    {id:createId(),name:'餐饮'},
-]
-const useTags = ()=>{
-    const [tags, setTags] = useState<{ id:number,name:string }[]>(defaultTags);
-    const findTag = (id:number) =>tags.filter(tag => tag.id ===id)[0]
-    return{tags,setTags,findTag}
-}
-export {useTags}
+    {id: createId(), name: '服饰'},
+    {id: createId(), name: '交通'},
+    {id: createId(), name: '通讯'},
+    {id: createId(), name: '餐饮'},
+];
+const useTags = () => { // 封装一个自定义 Hook
+    const [tags, setTags] = useState<{ id: number, name: string }[]>(defaultTags);
+    const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
+    const findTagIndex = (id: number) => {
+        let result = -1;
+        for (let i = 0; i < tags.length; i++) {
+            if (tags[i].id === id) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    };
+    const updateTag = (id: number, obj: { name: string }) => {
+        const index = findTagIndex(id);
+        // 深拷贝 tags 得到 tagsClone
+        const tagsClone = JSON.parse(JSON.stringify(tags));
+        // 把 tagsClone 的第 index 个删掉 ，替换成 {id:id,name:obj.name}
+        tagsClone.splice(index, 1, {id: id, name: obj.name});
+        setTags(tagsClone);
+    };
+    return {tags, setTags, findTag,updateTag,findTagIndex};
+};
+export {useTags};
 
